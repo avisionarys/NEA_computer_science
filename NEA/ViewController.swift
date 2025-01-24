9b810208225c413af5f3d890227ae35374b03e1a
avisionarys <18avisrolia@salesian.surrey.sch.uk>
2025-01-24 16:37:24 +0000

    Initial Commit

---
+++ NEA/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NEA
+//
+//  Created by CHETAN VISROLIA on 24/01/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ NEATests/NEATests.swift
@@ -0,0 +1,17 @@
+//
+//  NEATests.swift
+//  NEATests
+//
+//  Created by CHETAN VISROLIA on 24/01/2025.
+//
+
+import Testing
+@testable import NEA
+
+struct NEATests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
